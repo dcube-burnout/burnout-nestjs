de4863cb7fbbc4ef04a01ad7d092c1d5a2906114
ningyu <ningyu@estl.sg>
2023-02-03 18:42:32 +0800

    fix id not auto incrementing when seeding

--- src/database/seeders/seeder.ts
+++ src/database/seeders/seeder.ts
@@ -4,21 +4,25 @@ import { User } from 'src/users/entities/user.entity';
 import { Team } from 'src/teams/entities/team.entity';
 import { Progress, Session } from 'src/sessions/entities/session.entity';
 
-const teamData = [{ id: 1, leader_id: 1 }];
+const teamData = [{ leader_id: 1 }, { leader_id: 3 }];
 
 const userData = [
-  { id: 1, login: 'John', password: '123', team: 1 },
-  { id: 2, login: 'Jane', password: '456', team: 1 },
+  { login: 'John', password: '123', team: 1 },
+  { login: 'Jane', password: '456', team: 1 },
+  { login: 'Joey', password: '789', team: 2 },
 ];
 
 const sessionData = [
-  { id: 1, team_id: 1, title: 'Sprint 142', date: '02/03/2023', progress: Progress.inProgress },
+  { team: 1, title: 'Sprint 142', date: '02/03/2023', progress: Progress.done },
+  { team: 1, title: 'Sprint 143', date: '03/03/2023', progress: Progress.inProgress },
+  { team: 2, title: 'Sprint 143', date: '03/03/2023', progress: Progress.inProgress },
 ];
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    await em.upsertMany(Team, teamData);
-    await em.upsertMany(User, userData);
-    await em.upsertMany(Session, sessionData);
+    teamData.map((i) => em.create(Team, i));
+    userData.map((i) => em.create(User, i));
+    sessionData.map((i) => em.create(Session, i));
+    await em.flush();
   }
 }
