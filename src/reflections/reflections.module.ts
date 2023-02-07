ef5819dfe92155f8b341cf45ccd12d8e58d8e96a
ningyu <ningyu@estl.sg>
2023-02-07 23:40:03 +0800

    fix get by userid

--- src/reflections/reflections.module.ts
+++ src/reflections/reflections.module.ts
@@ -4,11 +4,10 @@ import { ReflectionsController } from './reflections.controller';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Reflection } from './entities/reflections.entity';
 import { BurnoutInv } from './entities/burnout-inv.entity';
-import { SessionsModule } from 'src/sessions/sessions.module';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Reflection, BurnoutInv]), SessionsModule, UsersModule],
+  imports: [MikroOrmModule.forFeature([Reflection, BurnoutInv]), UsersModule],
   controllers: [ReflectionsController],
   providers: [ReflectionsService],
   exports: [ReflectionsService],
--- src/reflections/reflections.service.ts
+++ src/reflections/reflections.service.ts
@@ -1,23 +1,19 @@
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
-import { SessionsService } from 'src/sessions/sessions.service';
 import { UsersService } from 'src/users/users.service';
 import { CreateReflectionDto } from './dto/create-reflection.dto';
 import { Reflection } from './entities/reflections.entity';
 import { BurnoutInv } from './entities/burnout-inv.entity';
 import { ReflectionsRepository } from './entities/reflections.repository';
-import { BurnoutInvRepository } from './entities/burnout-inv.repository';
 
 @Injectable()
 export class ReflectionsService {
   constructor(
     @InjectRepository(Reflection) private readonly reflectionRepository: ReflectionsRepository,
-    @InjectRepository(BurnoutInv) private readonly burnoutInvRepository: BurnoutInvRepository,
     private readonly userService: UsersService,
-    private readonly sessionsService: SessionsService,
   ) {}
   findByUser(id: number) {
-    return this.reflectionRepository.find({ id });
+    return this.reflectionRepository.find({ user: id });
   }
 
   findOneById(id: number) {
