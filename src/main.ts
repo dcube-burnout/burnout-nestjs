3be14b31c4bc77ba3aa929fc0ff425e16b317283
ningyu <ningyu@estl.sg>
2023-02-07 22:30:18 +0800

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder().setTitle('Burnout API server').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
