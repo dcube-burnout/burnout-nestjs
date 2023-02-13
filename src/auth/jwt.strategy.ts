de7e3b9f729d47eef3a4f7361c237b666c2e77e5
ningyuuu <ningyu321@gmail.com>
2023-02-13 14:57:38 +0800

    update jwt strategy to use config service

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,15 +1,15 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { jwtKey } from './constants';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtKey,
+      secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
     });
   }
 
